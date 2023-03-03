c4302f4ee8411cf2362202c8fce007c1b7c01d12
Taras Bobriienko <taras3189@gmail.com>
2023-03-03 10:34:59 +0200

    DarkTheme 04

--- lib/drawer/goals_screen.dart
+++ lib/drawer/goals_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../calendar.dart';
 import '../settings/settings_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '../ui/theme_provider.dart';
 
 class GoalsScreen extends StatelessWidget {
   static String route = '/goals';
@@ -11,100 +13,101 @@ class GoalsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.daily_goals),
-        centerTitle: false,
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const GoalsScreen(),
-                  ),
-                );
-              },
-              icon: const Icon(Icons.check_circle_outline)),
-          IconButton(
-              onPressed: () async {
-                await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SettingsScreen(),
-                  ),
-                );
-              },
-              icon: const Icon(Icons.settings_rounded)),
-          IconButton(
-              onPressed: () async {
-                await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const Calendar(),
-                  ),
-                );
-              },
-              icon: const Icon(Icons.calendar_month_sharp)),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(14.0),
-        child: Center(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        top: 60, bottom: 10, left: 10, right: 10),
-                    child: SizedBox(
-                      width: 120,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [Text('SET')],
+    return Consumer<ThemeProvider>(builder: (context, themeProvider, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(AppLocalizations.of(context)!.daily_goals),
+          centerTitle: false,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const GoalsScreen(),
+                    ),
+                  );
+                },
+                icon: const Icon(Icons.check_circle_outline)),
+            IconButton(
+                onPressed: () async {
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SettingsScreen(),
+                    ),
+                  );
+                },
+                icon: const Icon(Icons.settings_rounded)),
+            IconButton(
+                onPressed: () async {
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Calendar(),
+                    ),
+                  );
+                },
+                icon: const Icon(Icons.calendar_month_sharp)),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Center(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 60, bottom: 10, left: 10, right: 10),
+                      child: SizedBox(
+                        width: 120,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [Text('SET'), ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        top: 60, bottom: 10, left: 10, right: 10),
-                    child: SizedBox(
-                      width: 120,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey
-                        ),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [Text('CANCEL')],
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 60, bottom: 10, left: 10, right: 10),
+                      child: SizedBox(
+                        width: 120,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.grey),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [Text('CANCEL')],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              RichText(
-                text: const TextSpan(
-                  text: '\nNOT STARTED',
-                  style: TextStyle(
-                    color: Colors.blueGrey,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
+                  ],
+                ),
+                RichText(
+                  text: const TextSpan(
+                    text: '\nNOT STARTED',
+                    style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
